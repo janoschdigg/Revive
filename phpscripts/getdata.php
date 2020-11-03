b1893928a6b409e389837ef03d00397e08f159fe
Janosch Diggelmann <janosch.digg@outlook.com>
2020-11-03 20:16:32 +0100

    no message

--- phpscripts/getdata.php
+++ phpscripts/getdata.php
@@ -12,20 +12,20 @@ if(isset($_GET['id']))
 if($type == 'activity')
 {
 
-    echo json_encode(mb_convert_encoding($con->sqlexec("Select * from revive.activity order by date asc"), 'UTF-8', 'UTF-8'));
+    echo json_encode(($con->sqlexec("Select * from revive.activity order by date asc")));
 
 }
 if($type == 'church')
 {
-    echo json_encode(mb_convert_encoding($con->sqlexec("Select * from revive.church"), 'UTF-8', 'UTF-8'));
+    echo json_encode(($con->sqlexec("Select * from revive.church")));
 }
 if($type == 'detail' && isset($_GET['id']))
 {
-    echo json_encode(mb_convert_encoding($con->sqlexec("Select * from revive.activity where id = " . $id), 'UTF-8', 'UTF-8'));
+    echo json_encode(($con->sqlexec("Select * from revive.activity where id = " . $id)));
 }
 if($type == 'user' && isset($_GET['id']))
 {
-    echo json_encode(mb_convert_encoding($con->sqlexec("Select * from revive.user where id = " . $id), 'UTF-8', 'UTF-8'));
+    echo json_encode($con->sqlexec("Select * from revive.user where id = " . $id));
 }
 
 ?>
